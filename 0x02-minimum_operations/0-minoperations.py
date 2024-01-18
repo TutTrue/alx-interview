26beff98faea5a4c86a0719977a1f6fcbbe4bc44
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-18 19:34:05 +0200

    Minimum Operations if n if div by 2 numbers choice the bigger

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,15 +9,8 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    if n < 6:
-        return n
-    if n % 5 == 0:
-        return 5 + n//5
-    if n % 4 == 0:
-        return 4 + n//4
-    elif n % 3 == 0:
-        return 3 + n//3
-    elif n % 2 == 0:
-        return 2 + n//2
-    else:
-        return 1 + n
+    m = float("inf")
+    for i in range(1, n//2+1):
+        if n % i == 0:
+            m = min(m, i + n//i)
+    return m
